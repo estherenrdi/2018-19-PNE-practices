e5110af36bbf4a4988e7e77a8e567fd78b9e969e
esthered <estherenriquez04 @gmail.com>
2019-02-06 18:27:56 +0100

    The main program should ask the user for the sequence, print the total length, the number of bases and their percentage. But we import a function from Bases.py

---
+++ session-5/example-1-2.py
@@ -0,0 +1,38 @@
+def count_letter(sequence, lett):
+    count_let = sequence.count(lett)
+    return count_let
+
+
+from Bases import count_bases
+
+def percentage(seq):
+    tl = len(seq)
+    list_per = list()
+    if tl > 0:
+        n_a = round(100.0 * count_bases(seq)['A'] / tl, 1)
+        list_per.append(n_a)
+        n_t = round(100.0 * count_bases(seq)['T'] / tl, 1)
+        list_per.append(n_t)
+        n_c = round(100.0 * count_bases(seq)['C'] / tl, 1)
+        list_per.append(n_c)
+        n_g = round(100.0 * count_bases(seq)['G'] / tl, 1)
+        list_per.append(n_g)
+
+    else:
+        perc = 0
+
+    return list_per
+
+
+def main_programe():
+    seq = str(input("Enter the sequence : ").upper())
+
+    print("This sequence is {} bases in lenght".format(len(seq)))
+
+    print("Base A\n\tCounter: " ,count_bases(seq)['A'],"\n\tPercentage :",percentage(seq)[0])
+    print("Base T\n\tCounter: " ,count_bases(seq)['T'],"\n\tPercentage :",percentage(seq)[1])
+    print("Base C\n\tCounter: " ,count_bases(seq)['C'],"\n\tPercentage :",percentage(seq)[2])
+    print("Base G\n\tCounter: " ,count_bases(seq)['G'],"\n\tPercentage :",percentage(seq)[3])
+
+
+print(main_programe())
