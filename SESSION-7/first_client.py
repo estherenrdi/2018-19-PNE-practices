2c4dfd23e13a2a1383bd190e4a8376383a3cf41d
esthered <estherenriquez04 @gmail.com>
2019-02-12 16:40:32 +0100

    here it is my first client

---
+++ SESSION-7/first_client.py
@@ -0,0 +1,21 @@
+# A sockect is an object for communicate
+# Programming our first client
+import socket
+# Create a socket for communicating with the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("socket created")
+
+PORT = 8080
+IP = "212.128.253.64"
+s.connect((IP, PORT))
+
+s.send(str.encode("Adrian atiende"))
+
+msg = s.recv(2048).decode("utf-8")
+print("This is the messege from the server")
+print(msg)
+
+s.close()
+
+print("The end")
