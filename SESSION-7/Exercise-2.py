3d3d149a54215bff837b6c12650ae121b12fa8cb
esthered <estherenriquez04 @gmail.com>
2019-02-13 17:36:01 +0100

    a client that is a chat

---
+++ SESSION-7/Exercise-2.py
@@ -0,0 +1,24 @@
+# A sockect is an object for communicate
+# Programming our first client
+import socket
+# Create a socket for communicating with the server
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+    PORT = 8083
+    IP = "212.128.253.64"
+    s.connect((IP, PORT))
+
+    mensaje = input("Please, enter here your messege : ")
+    s.send(str.encode(mensaje))
+
+    msg = s.recv(2048).decode("utf-8")
+    print("This is the messege from the server")
+    print(msg)
+
+    s.close()
+
+
+
