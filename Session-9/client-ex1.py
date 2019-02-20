0d0204444664a7d25973861bafb7650354f45317
esthered <estherenriquez04 @gmail.com>
2019-02-20 17:28:06 +0100

    This is my client for exercice 1. If you enter EXIT it finishes the program

---
+++ Session-9/client-ex1.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+# SERVER IP, PORT
+IP = "212.128.253.76"
+PORT = 8089
+
+while True:
+    msg = input("> ")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if msg != 'EXIT':
+        # establish the connection to the Server (IP, PORT)
+        s.connect((IP, PORT))
+
+        # Send the request message to the server
+        s.send(str.encode(msg))
+
+        # Receive the servers respoinse
+        response = s.recv(2048).decode()
+
+        # Print the server's response
+        print("Response: {}".format(response))
+
+        s.close()
+    elif msg == 'EXIT':
+        sys.exit()
